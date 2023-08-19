3fc99f5cf25193fe72ef17dd59495950ac1f9b70
Douglas Wesley <douglas.almeida55@yahoo.com.br>
2023-08-19 02:31:02 -0300

    Update v1.0.6
    - Finish fifth section

---
+++ 5_FORM_EM_REACT/form/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import MyForm from './components/MyForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Forms</h2>
+      <MyForm user={{ 
+        name: "Josias", 
+        email: 'josias@gmail.com', 
+        bio: 'Sou advogado', 
+        role: 'admin' }} 
+      />
+    </div>
+  );
+}
+
+export default App;
